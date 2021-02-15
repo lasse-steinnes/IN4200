d8ff922b4220b9c6433cdad14ebb4c0ffa781056
Lasse Steinnes <lasse.steinnes93@gmail.com>
2021-02-15 15:48:40 +0100

    added instructor sols

---
+++ exercise-set-5/main.cpp
@@ -0,0 +1,10 @@
+main(int argc, char const *argv[]){
+  int exercise;
+  cout << "Press 1 to run ex.1 and 2 \n";
+  cout << "Press 2 to run ex.3 \n";
+  cout << "Press 3 to verify openmp (4 cores) \n";
+  cout << "Enter number:" << " ";
+  cin >> exercise;
+
+  return 0;
+}
