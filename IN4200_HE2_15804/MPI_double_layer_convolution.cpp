baeebcd8fed2188e13dfad7ab9ae2f2311746368
Lasse Steinnes <lasse.steinnes93@gmail.com>
2021-05-04 12:18:18 +0200

    Fixed after crash
    Atom editor crashed. Always push functional changes every 10 mins or so!!

--- IN4200_HE2_15804/MPI_double_layer_convolution.cpp
+++ IN4200_HE2_15804/MPI_double_layer_convolution.cpp
@@ -5,7 +5,7 @@
 #include <stdlib.h> //malloc
 
 void divide_work(int **out_send_counts, int **rows, int **send_counts, int **send_disp, int **gather_disp,
-                  int K1, int K2, int M, int N, int procs, int my_rank){
+                  int **subrowsB_arr, int K1, int K2, int M, int N, int procs, int my_rank){
 
 // divide work //
 //allocate arrays
@@ -23,43 +23,50 @@ void divide_work(int **out_send_counts, int **rows, int **send_counts, int **sen
 // array for number of elements in gathering output
 *out_send_counts = (int*) malloc(procs*sizeof *out_send_counts);
 
+// array for number of rows in intermediate B matrix
+*subrowsB_arr = (int*) malloc(procs*sizeof *subrowsB_arr);
 
-int intermediate_rows = M - K1 + 1; // number of rows in intermediate matrix
-int intermediate_sub_rows = intermediate_rows/procs; // integer division
-int remaind = intermediate_rows%procs; // with remainder
-int sub_rows = K1 + 1*(intermediate_sub_rows-1); // sub rows in input array
+//int rowsA = M;
+//int rowsB = M - K1 + 1;
+int cols_out = N-K1-K2+2;
 
-// to be used to gathering the final result
-int output_rows = M - K1 - K2 + 2;
-int output_sub_rows = output_rows/procs;
-int remaind2 = output_rows%procs;
+int out_rows = M - K1 - K2 + 2;
+int sub_rows_out = out_rows/procs;
+int remaind = out_rows%procs;
 
+int  sub_B =  K1 + 1*(sub_rows_out-1); // number of rows in intermediate matrix
+//int  sub_rows_input = K2 + 1*(sub_rows_B-1); // sub rows in input array
 
 (*send_disp[0]) = 0;
 (*gather_disp[0]) = 0;
 
-printf("sub_rows: %d, remaind: %d, rank: %d \n", sub_rows, remaind, my_rank);
+printf("sub_rows: %d, remaind: %d, rank: %d \n", sub_rows_out, remaind, my_rank);
 
 // divide work by filling in arrays to be send to scatterv and gatherv
 // Last remainder processes gets an extra row.
 //int placeholder1, placeholder2;
 // gatherdisp must be suited to output
+int tot_rows = 0,sub_rows_B = 0;
 for (int rankid = 0; rankid < procs-1; rankid++){
-
-  (*rows)[rankid] = sub_rows + ((rankid >= (procs - remaind)) ? 1:0); // number of rows to be sent
-  (*send_counts[rankid]) = (*rows)[rankid]*N; //send numbercounter of elements
-  (*send_disp)[rankid+1] = (*send_disp)[rankid] + (*rows)[rankid]*N; // send the index to start
-  (*gather_disp)[rankid+1] = (*gather_disp)[rankid] + output_sub_rows  // how displacement is in gather
-                              +  ((rankid >= (procs - remaind2)) ? 1:0); // while gathering output results
-  (*out_send_counts)[rankid] = (output_sub_rows                        // number of elements to be gathered
-                               + ((rankid >= (procs - remaind2)) ? 1:0))*(N-K1-K2+2); // to outputmatrix
+  sub_rows_B = sub_B + ((rankid >= (procs - remaind)) ? 1:0);
+  (*rows)[rankid] =  K2 + 1*(sub_rows_B-1);        // number of rows to be sent
+  (*subrowsB_arr)[rankid] = sub_rows_B;             // number of rows in intermediate matrix
+  (*send_counts[rankid]) = (*rows)[rankid]*N;     //send numbercounter of elements
+  tot_rows += sub_rows_out + ((rankid >= (procs - remaind)) ? 1:0); // counter how to distr
+  (*send_disp)[rankid+1] = tot_rows*N;            // send the index to start
+  (*gather_disp)[rankid+1] = (*gather_disp)[rankid] + sub_rows_out  // how displacement is in gather
+                              +  ((rankid >= (procs - remaind)) ? 1:0); // while gathering output results
+  (*out_send_counts)[rankid] = (sub_rows_out                       // number of elements to be gathered
+                               + ((rankid >= (procs - remaind)) ? 1:0))*(cols_out); // to outputmatrix
 }
 
 // fix the last ones
-(*rows)[procs-1] = sub_rows + ((procs-1) >= (procs - remaind) ? 1:0);
+sub_rows_B = sub_B + ((procs-1 >= (procs - remaind)) ? 1:0);
+(*rows)[procs-1] =  K2 + 1*(sub_rows_B-1)
+                  + ((procs-1) >= (procs - remaind) ? 1:0);
 (*send_counts)[procs-1] = (*rows)[procs-1]*N;
-(*out_send_counts)[procs-1] = (output_sub_rows                        // number of elements to be gathered
-                          + ((procs-1 >= (procs - remaind2)) ? 1:0))*(N-K1-K2+2); // to outputmatrix
+(*out_send_counts)[procs-1] = (sub_rows_out                      // number of elements to be gathered
+                          + ((procs-1 >= (procs - remaind)) ? 1:0))*(cols_out); // to outputmatrix
 }
 
 void MPI_double_layer_convolution(int M, int N, float **input, int K1,
@@ -71,11 +78,11 @@ MPI_Comm_rank (MPI_COMM_WORLD, &my_rank); // get ID of current process
 
 int *out_send_counts = NULL,*rows = NULL,
     *send_counts = NULL, *send_disp = NULL,
-    *gather_disp = NULL;
+    *gather_disp = NULL, *sub_rows_B = NULL;
 
 // only divide work once
 divide_work(&out_send_counts,&rows, &send_counts, &send_disp, &gather_disp,
-            K1, K2, M, N, procs,my_rank);
+            &sub_rows_B,K1, K2, M, N, procs,my_rank);
 
 // allocate local buffers and scatterv (input and output)
 float *intermediate;
@@ -124,7 +131,7 @@ for (int i = 0; i < procs; i++){
 
 
 // printing the scattered array
-printf("rank %d rows[my_rank] %d  N %d \n", my_rank, rows[my_rank], N);
+printf("rank %d rows[my_rank] %d  N %d \n", my_rank, rows[my_rank], N);
 for (int i = 0; i < rows[my_rank]; i++){
   for (int j = 0; j < N; j++){
     printf("%f ", input[i][j]);
@@ -142,7 +149,6 @@ for (int i = 0; i < rows[my_rank]; i++){
 // Allocate local buffers.
 double *A;
 double *x = malloc(N * sizeof *x);
-
 } else (not root) {
     A = malloc(M*rows[myrank] * sizeof *A);
 }*/
@@ -171,6 +177,5 @@ double *x = malloc(N * sizeof *x);
                 MPI_DOUBLE,
                 0,
                 MPI_COMM_WORLD);
-
 */
 }
